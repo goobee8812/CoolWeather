3d08c5fb5b9db9fe154855d7de303233eea5280f
goobee <goobee_yuer0716@163.com>
2017-05-17 17:06:49 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/administrator/coolweather/util/ChooseAreaFragment.java
+++ app/src/main/java/com/example/administrator/coolweather/util/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.administrator.coolweather.util;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.administrator.coolweather.R;
+import com.example.administrator.coolweather.WeatherActivity;
 import com.example.administrator.coolweather.db.City;
 import com.example.administrator.coolweather.db.County;
 import com.example.administrator.coolweather.db.Province;
@@ -101,6 +103,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCountries();//进入县城
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
